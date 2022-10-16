f8ad8bc061c68db68a59bffe504b40422438d97d
Nicolas Morel <nicolas@morel.io>
2022-10-16 23:55:55 +0200

    chore: add hapi eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         es2021: true,
         node: true
     },
-    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:@hapi/module', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2021,
@@ -14,6 +14,7 @@ module.exports = {
     plugins: ['@typescript-eslint', 'prettier'],
     rules: {
         '@typescript-eslint/no-explicit-any': 0,
+        '@hapi/scope-start': 0,
         'prettier/prettier': 'error'
     }
 };
