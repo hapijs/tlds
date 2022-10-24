4257ad5dbb78dd7cb92a6ee6313baebd15982797
Nicolas Morel <nicolas@morel.io>
2022-10-24 22:31:13 +0200

    chore: change trailing comma behavior

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
-        node: true
+        node: true,
     },
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:@hapi/module', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2021,
-        sourceType: 'module'
+        sourceType: 'module',
     },
     plugins: ['@typescript-eslint', 'prettier'],
     rules: {
         '@typescript-eslint/no-explicit-any': 0,
         '@hapi/scope-start': 0,
-        'prettier/prettier': 'error'
-    }
+        'prettier/prettier': 'error',
+    },
 };
