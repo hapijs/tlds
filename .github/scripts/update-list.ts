f4cd5f5bab529ab9285b10df5802b20400735036
Nicolas Morel <nicolas@morel.io>
2024-03-28 23:05:06 +0100

    feat: add auto-update of tlds list
    closes #6

---
+++ .github/scripts/update-list.ts
@@ -0,0 +1,66 @@
+import { execFileSync } from 'node:child_process';
+import { writeFile } from 'node:fs/promises';
+import { createInterface } from 'node:readline';
+import * as Wreck from '@hapi/wreck';
+
+const ianaUri = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt';
+
+async function fetchTLDs() {
+    const request = await Wreck.get(ianaUri);
+    const rl = createInterface({
+        input: Wreck.toReadableStream(request.payload as string),
+        crlfDelay: Infinity,
+    });
+
+    let lineNumber = 0;
+
+    const data = {
+        version: '',
+        domains: [] as string[],
+    };
+
+    for await (const line of rl) {
+        if (lineNumber === 0) {
+            if (/^# Version \d{8}, Last Updated .+ UTC$/.test(line)) {
+                throw new Error('Unexpected version line');
+            }
+            data.version = line;
+        } else if (line.length > 0) {
+            data.domains.push(line);
+        }
+
+        lineNumber++;
+    }
+
+    const formattedFile = `// ${ianaUri}
+// ${data.version}
+
+export const TLDS: string[] = [
+${data.domains.map((d) => `    '${d}',`).join('\n')}
+];
+`;
+
+    try {
+        execFileSync(
+            'git',
+            [
+                'diff',
+                '--exit-code', // Fail if there are changes
+                '--no-index',
+                '-I',
+                '^// #', // Ignore version line, it changes every day
+                '--',
+                'src/tlds.ts',
+                '-', // Take stdin
+            ],
+            {
+                input: formattedFile,
+            },
+        );
+    } catch {
+        // An error means the file is probably different
+        await writeFile('./src/tlds.ts', formattedFile);
+    }
+}
+
+fetchTLDs();
